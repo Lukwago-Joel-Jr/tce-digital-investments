ea40e9ad778f8b9946821d8db3841ce3d84c014b
Joel Jr <jjt200220@gmail.com>
2025-07-11 13:44:24 +0200

    added meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,30 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Wealth Builder Academy",
   description: "Wealth Builder Academy you're way to digital wealth",
+  keywords: "digital wealth, online courses, financial freedom, entrepreneurship, skills development",
+  authors: [{ name: "Wealth Builder Academy", url: "https://www.tcedigitalinvestments.com" }],
+  creator: "Wealth Builder Academy",
+  openGraph: {
+    title: "Wealth Builder Academy",
+    description: "Learn the skills to build digital wealth and freedom.",
+    url: "https://www.tcedigitalinvestments.com",
+    siteName: "Wealth Builder Academy",
+    images: [
+      {
+        url: "/images/logo-512x512.png",
+        width: 1200,
+        height: 630,
+        alt: "Wealth Builder Academy OG Image"
+      }
+    ],
+    locale: "en_US",
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@wealthbuilderacademy",
+    creator: "@wealthbuilderacademy"
+  },  
   icons: {
     icon: '/favicon.ico'}
 };
