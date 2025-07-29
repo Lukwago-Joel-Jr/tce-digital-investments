3f123056c72b0cf489e10a01ce156900cab4a1c7
Contecfy <99560679+joeljrthe1st@users.noreply.github.com>
2025-07-29 13:21:48 +0300

    renamed

--- src/app/wealth-builder-program/page.js
+++ src/app/wealth-builder-program/page.js
@@ -63,7 +63,7 @@ export const metadata = {
     siteName: "Wealth Builder Academy",
     images: [
       {
-        url: "/images/boss2-1200x630.jpeg",
+        url: "/images/sandraboss.jpeg",
         width: 1200,
         height: 630,
         alt: "Wealth Builder Program OG Image",
