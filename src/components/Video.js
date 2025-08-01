28479b322c9a5b79777240b177b892ddf15be13a
Contecfy <99560679+joeljrthe1st@users.noreply.github.com>
2025-08-01 16:48:40 +0300

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -27,16 +27,34 @@
 
 "use client";
 
+import { Pacifico, Meow_Script } from "next/font/google";
+import { FaChessRook } from "react-icons/fa";
+import { FaCircleCheck } from "react-icons/fa6";
+
+const pacifico = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const meow = Meow_Script({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function VideoSection() {
   return (
-    <div className="md:h-screen">
-      <section className="relative bg-white py-16 px-4 md:px-10 flex justify-center items-center ">
-        {/* Background shadow/layer */}
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div className="w-[90%] md:w-[720px] h-[calc(100%-3rem)] bg-gray-100 rounded-3xl shadow-md translate-x-10 translate-y-6"></div>
+    <div className="md:h-screen flex flex-col justify-center align-center">
+      <section className="relative bg-white py-16 px-4 md:px-10 flex flex-col  items-center ">
+        <div className=" mb-4 md:w-4xl max-w-4xl flex justify-start">
+          <div className="-rotate-30  w-50 flex items-center ">
+            <FaChessRook size={28} className="text-green-200" />
+            <p
+              className={`${meow.className} text-4xl md:text-4xl font-light text-gray `}
+            >
+              My Story
+            </p>
+          </div>
         </div>
-
-        {/* Foreground content */}
         <div className="relative max-w-3xl w-full z-10">
           <div className="aspect-video w-full rounded-3xl overflow-hidden shadow-xl border border-gray-200">
             <video
@@ -53,6 +71,15 @@ export default function VideoSection() {
               />
               Your browser does not support the video tag.
             </video>
+          </div>
+          <div className="mt-2 max-w-4xl flex justify-end">
+            <div className="-rotate-30 w-40">
+              <p
+                className={`${meow.className} text-4xl md:text-4xl font-light `}
+              >
+                Sandra
+              </p>
+            </div>
           </div>
         </div>
       </section>
