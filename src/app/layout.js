f1c1ba8d9284ef863e42a0e48e1c8a07f6224773
Lukwago Joel Jr <contecfy@gmail.com>
2025-09-19 15:30:35 +0300

    added script from mailchimp

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,32 @@
-import { Geist, Geist_Mono, Parisienne, Baskervville, Inter, Host_Grotesk,Cormorant_Infant } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Parisienne,
+  Baskervville,
+  Inter,
+  Host_Grotesk,
+  Cormorant_Infant,
+} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Nav";
 import Footer from "@/components/Footer";
 
 const parisienne = Parisienne({
   variable: "--font-parisienne",
-  weight: '400',
+  weight: "400",
   subsets: ["latin"],
 });
 const montserrat = Host_Grotesk({
-  weight: ['400', '800'],
+  weight: ["400", "800"],
   subsets: ["latin"],
 });
 const cormorat = Cormorant_Infant({
-  weight:'400',
+  weight: "400",
   subsets: ["latin"],
 });
 
 const baskervville = Baskervville({
-  weight:'400',
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -32,13 +40,18 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
 export const metadata = {
-  metadataBase: new URL('https://www.tcedigitalinvestments.com'), // ✅ Add this line
+  metadataBase: new URL("https://www.tcedigitalinvestments.com"), // ✅ Add this line
   title: "Wealth Builder Academy",
   description: "Wealth Builder Academy — your way to digital wealth",
-  keywords: "digital wealth, online courses, financial freedom, entrepreneurship, skills development",
-  authors: [{ name: "Wealth Builder Academy", url: "https://www.tcedigitalinvestments.com" }],
+  keywords:
+    "digital wealth, online courses, financial freedom, entrepreneurship, skills development",
+  authors: [
+    {
+      name: "Wealth Builder Academy",
+      url: "https://www.tcedigitalinvestments.com",
+    },
+  ],
   creator: "Wealth Builder Academy",
   openGraph: {
     title: "Wealth Builder Academy",
@@ -50,32 +63,39 @@ export const metadata = {
         url: "/images/original.png",
         width: 1200,
         height: 630,
-        alt: "Wealth Builder Academy OG Image"
-      }
+        alt: "Wealth Builder Academy OG Image",
+      },
     ],
     locale: "en_US",
-    type: "website"
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     site: "@wealthbuilderacademy",
-    creator: "@wealthbuilderacademy"
-  },  
+    creator: "@wealthbuilderacademy",
+  },
   icons: {
-    icon: '/favicon.ico'
-  }
+    icon: "/favicon.ico",
+  },
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.className} antialiased`}
-      >
-        <Navbar/>
+      <body className={`${montserrat.className} antialiased`}>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
+        <Script
+          id="mcjs"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(c,h,i,m,p){
+              m=c.createElement(h),p=c.getElementsByTagName(h)[0],
+              m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+            }(document,"script","https://chimpstatic.com/mcjs-connected/js/users/f5d80c08e45124cb703562330/bd601dad58b12f0d4f9964a79.js");`,
+          }}
+        />
       </body>
     </html>
   );
