6e87ad7cc2e3b008afa5106b6f5ef83fd42ddde5
contecfy <contecfy@gmail.com>
2025-10-17 21:00:34 +0300

    reduced the book price for testing purporses

--- src/components/Data/ebooks.js
+++ src/components/Data/ebooks.js
@@ -347,7 +347,8 @@ export const products = [
     shortDescription:
       "Join our comprehensive program to master wealth building strategies.",
 
-    price: 342,
+    // price: 342,
+    price: 0.15,
     cover: "/images/ACADEMY.png",
     metacover: "/images/academyog.png",
   },
@@ -392,7 +393,8 @@ export const products = [
     cover: "/images/KINGDOMLENDING.png",
     metacover: "/images/kingdomog.png",
 
-    price: 84,
+    // price: 84,
+    price: 0.15,
     author: "Sandra N Sserumaga",
     pages: 59,
     publisher: "Wealth Build Press",
@@ -482,7 +484,8 @@ export const products = [
     cover: "/images/DIGITALENTREPRENUURSHIP1.png",
     metacover: "/images/digitalog.png",
 
-    price: 27,
+    // price: 27,
+    price: 0.15,
     author: "Sandra N Sserumaga",
     pages: 35,
     publisher: "Wealth Build Press",
