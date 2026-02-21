9686d209b24c8c0ba15992f10f0950bb9453f8cb
contecfy <contecfy@gmail.com>
2026-02-21 11:27:15 +0300

    put it back

--- src/components/Data/ebooks.js
+++ src/components/Data/ebooks.js
@@ -56,7 +56,7 @@ export const products = [
     cover: "/images/KINGDOMLENDING.png",
     metacover: "/images/kingdomog.png",
 
-    price: 0.15,
+    price: 84,
     author: "Sandra N Sserumaga",
     pages: 59,
     publisher: "Wealth Build Press",
